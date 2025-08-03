b525976ea282f9e33424473380e02157270f1863
HwangJeongSeong <hjs54336297@gmail.com>
2025-08-04 03:18:44 +0900

    create table by HTML

---
+++ src/main/java/com/mysite/sbb2/answer/AnswerController.java
@@ -0,0 +1,10 @@
+package com.mysite.sbb2.answer;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class AnswerController {
+
+}
